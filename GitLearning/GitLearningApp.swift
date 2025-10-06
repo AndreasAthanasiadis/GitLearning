3d316cc308bc711327d7a1eef5a55d548d8ecd67
Andreas Athanansiadis <andreas_athanasiadis@icloud.com>
2025-10-06 11:47:20 +0300

    Initial Commit

---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Andreas Athanasiadis on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
